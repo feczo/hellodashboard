b289f5cb2384dbe61f33453339d4e067b92f1975
Szabolcs Feczak <szabolcs.feczak@gmail.com>
2014-07-02 20:28:46 +0100

    addressing reviers comments

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import bqclient
 import webapp2
 import logging
+from gviz_data_table import Table
+from gviz_data_table import encode
 from django.utils import simplejson as json
 from google.appengine.ext.webapp.template import render
 from oauth2client.appengine import oauth2decorator_from_clientsecrets
@@ -19,10 +21,10 @@ decorator = oauth2decorator_from_clientsecrets(
     cache=memcache)
 
 # Project ID for a project where you and your users
-#   are viewing members.  This is where the bill will be sent.
-#   During the limited availability preview, there is no bill.
+# are viewing members.  This is where the bill will be sent.
+# During the limited availability preview, there is no bill.
 # Replace this value with the Client ID value from your project,
-#   the same numeric value you used in client_secrets.json
+# the same numeric value you used in client_secrets.json
 BILLING_PROJECT_ID = "475473128136"
 DATA_PROJECT_ID = "publicdata"
 DATASET = "samples"
@@ -38,31 +40,24 @@ mem = memcache.Client()
 
 class MainPage(webapp2.RequestHandler):
     def _bq2geo(self, bqdata):
-        """geodata output for region maps must be in the format region, value.
-           Assume the query output is in this format, get names from schema.
-        """
+        # geodata output for region maps must be in the format region, value.
+        # Assume the query output is in this format, get names from schema.
         logging.info(bqdata)
-        columnNameGeo = bqdata["schema"]["fields"][0]["name"]
-        columnNameVal = bqdata["schema"]["fields"][1]["name"]
-        logging.info("Column Names=%s, %s" % (columnNameGeo, columnNameVal))
-        geodata = {}
-        geodata["rows"] = []
-        geodata["cols"] = (
-            {"id": columnNameGeo, "label": columnNameGeo, "type": "string"},
-            {"id": columnNameVal, "label": columnNameVal, "type": "number"})
+        table = Table()
+        NameGeo = bqdata["schema"]["fields"][0]["name"]
+        NameVal = bqdata["schema"]["fields"][1]["name"]
+        table.add_column(NameGeo, unicode, NameGeo)
+        table.add_column(NameVal, float, NameVal)
         for row in bqdata["rows"]:
-            newrow = ({"c": []})
-            newrow["c"].append({"v": "US-"+row["f"][0]["v"]})
-            newrow["c"].append({"v": row["f"][1]["v"]})
-            geodata["rows"].append(newrow)
+            table.append(["US-"+row["f"][0]["v"], float(row["f"][1]["v"])])
         logging.info("FINAL GEODATA---")
-        logging.info(geodata)
-        return json.dumps(geodata, ensure_ascii=True)
+        logging.info(table)
+        return encode(table)
 
     @decorator.oauth_required
     def get(self):
         data = mem.get('natality')
-        if not(data):
+        if not data:
             bq = bqclient.BigQueryClient(decorator)
             values = self._bq2geo(bq.Query(QUERY, BILLING_PROJECT_ID))
             data = {'data': values,
